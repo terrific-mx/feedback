a07aa46972fb55cd8231697346401e9b91504e3a
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-05-28 15:09:50 -0600

    wip

--- resources/views/pages/posts/create.blade.php
+++ resources/views/pages/posts/create.blade.php
@@ -1,3 +1,17 @@
-<div>
-    //
-</div>
+<?php
+
+use Livewire\Volt\Component;
+
+new class extends Component {
+
+}; ?>
+
+<x-layouts.app>
+    @volt('pages.posts.create')
+        <form class="space-y-6">
+            <flux:input label="Title" />
+            <flux:textarea label="Description" />
+            <flux:button variant="primary">Create</flux:button>
+        </form>
+    @endvolt
+</x-layouts.app>
