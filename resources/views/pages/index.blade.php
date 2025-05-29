9a7ae5cdd5c4c5605ef08827512fdab393221d36
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-05-28 18:21:58 -0600

    wip

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -16,7 +16,7 @@ new class extends Component {
 <x-layouts.board>
     @volt('pages.index')
         <div>
-            <div class="sm:border-b border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800">
+            <div id="secondary-header" class="sm:border-b border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800">
                 <div class="max-w-7xl px-6 sm:px-8 py-3 mx-auto flex flex-col sm:flex-row items-stretch sm:items-center gap-3 sm:gap-2">
                     <div class="max-sm:hidden flex items-baseline gap-3">
                         <flux:heading size="lg" class="text-lg">{{ __('Posts') }}</flux:heading>
@@ -26,7 +26,7 @@ new class extends Component {
                 </div>
             </div>
             <div class="min-h-4 sm:min-h-10"></div>
-            <div class="mx-auto max-w-lg max-sm:px-2">
+            <div id="posts" class="mx-auto max-w-lg max-sm:px-2">
                 @foreach ($posts as $post)
                     <div class="p-3 sm:p-4 rounded-lg">
                         <div>
--- resources/views/pages/posts/create.blade.php
+++ resources/views/pages/posts/create.blade.php
@@ -30,10 +30,20 @@ new class extends Component {
 
 <x-layouts.board>
     @volt('pages.posts.create')
-        <form wire:submit="create" class="space-y-6">
-            <flux:input wire:model="title" label="Title" />
-            <flux:textarea wire:model="description" label="Description" />
-            <flux:button type="submit" variant="primary">Create</flux:button>
-        </form>
+        <div>
+            <div id="secondary-header" class="sm:border-b border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800">
+                <div class="max-w-7xl px-6 sm:px-8 py-3 mx-auto flex flex-col sm:flex-row items-stretch sm:items-center gap-3 sm:gap-2">
+                    <div class="max-sm:hidden flex">
+                        <flux:heading size="lg" class="text-lg">{{ __('New post') }}</flux:heading>
+                    </div>
+                </div>
+            </div>
+            <div class="min-h-4 sm:min-h-10"></div>
+            <form id="form" wire:submit="create" class="mx-auto max-w-lg max-sm:px-2 space-y-6">
+                <flux:input wire:model="title" label="Title" />
+                <flux:textarea wire:model="description" label="Description" />
+                <flux:button type="submit" variant="primary">Create</flux:button>
+            </form>
+        </div>
     @endvolt
 </x-layouts.board>
