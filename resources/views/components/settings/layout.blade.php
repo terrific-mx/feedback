b6a3168a0ddc4c27328cd91992ddc2a886e7f023
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-05-28 19:34:56 -0600

    wip

--- resources/views/components/settings/layout.blade.php
+++ resources/views/components/settings/layout.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-start max-md:flex-col">
+<div class="flex items-start max-md:flex-col mx-auto max-w-7xl px-6 sm:px-8">
     <div class="me-10 w-full pb-4 md:w-[220px]">
         <flux:navlist>
             <flux:navlist.item :href="route('settings.profile')" wire:navigate>{{ __('Profile') }}</flux:navlist.item>
--- resources/views/partials/settings-heading.blade.php
+++ resources/views/partials/settings-heading.blade.php
@@ -1,5 +1,8 @@
-<div class="relative mb-6 w-full">
-    <flux:heading size="xl" level="1">{{ __('Settings') }}</flux:heading>
-    <flux:subheading size="lg" class="mb-6">{{ __('Manage your profile and account settings') }}</flux:subheading>
-    <flux:separator variant="subtle" />
+<div class="sm:border-b border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800">
+    <div class="max-w-7xl px-6 sm:px-8 py-3 mx-auto flex flex-col sm:flex-row items-stretch sm:items-center gap-3 sm:gap-2">
+        <div class="max-sm:hidden flex items-baseline gap-3">
+            <flux:heading size="lg" class="text-lg">{{ __('Settings') }}</flux:heading>
+        </div>
+    </div>
 </div>
+<div class="min-h-4 sm:min-h-10"></div>
