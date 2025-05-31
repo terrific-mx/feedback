e2998c0a3cdd8d68fe6420f3febfee0efc8bb3d8
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-05-31 09:44:57 -0600

    wip

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -3,7 +3,7 @@
     <head>
         @include('partials.head')
     </head>
-    <body class="min-h-screen bg-white dark:bg-zinc-800">
+    <body class="min-h-screen bg-white dark:bg-zinc-800 antialiased">
         <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900 flex items-center">
             <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
 
