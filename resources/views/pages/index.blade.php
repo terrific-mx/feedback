3cc82e0143e55d3fa56f260d7ff926d839f09de9
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-06-04 13:50:21 -0600

    wip

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -50,58 +50,46 @@ new class extends Component {
 
 <x-layouts.board>
     @volt('pages.index')
-        <div>
-            <div id="secondary-header" class="sm:border-b border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800">
-                <div class="max-w-7xl px-6 sm:px-8 py-3 mx-auto flex flex-col sm:flex-row items-stretch sm:items-center gap-3 sm:gap-2">
-                    <div class="max-sm:hidden flex items-baseline gap-3">
-                        <flux:heading size="lg" class="text-lg">
-                            @if ($this->currentBoard)
-                            {{ $this->currentBoard->name }}
-                            @else
-                            {{ __('All feedback') }}
-                            @endif
-                        </flux:heading>
-                        <flux:text>{{ $this->posts->count() }}</flux:text>
-                    </div>
-                    <flux:spacer />
-
-                    <div class="flex items-center gap-2">
-                        <flux:select wire:model.live="board" variant="listbox" class="sm:max-w-fit">
-                            <x-slot name="trigger">
-                                <flux:select.button size="sm">
-                                    <flux:icon.funnel variant="micro" class="mr-2 text-zinc-400" />
-                                    <flux:select.selected />
-                                </flux:select.button>
-                            </x-slot>
-                            <flux:select.option value="all" selected>{{ __('All') }}</flux:select.option>
-                            @foreach ($this->boards as $board)
-                            <flux:select.option :value="$board->id">{{ $board->name }}</flux:select.option>
-                            @endforeach
-                        </flux:select>
-
-                        <flux:select wire:model.live="sort" variant="listbox" class="sm:max-w-fit">
-                            <x-slot name="trigger">
-                                <flux:select.button size="sm">
-                                    <flux:icon.arrows-up-down variant="micro" class="mr-2 text-zinc-400" />
-                                    <flux:select.selected />
-                                </flux:select.button>
-                            </x-slot>
-                            <flux:select.option value="top" selected>{{ __('Top') }}</flux:select.option>
-                            <flux:select.option value="newest">{{ __('Newest') }}</flux:select.option>
-                        </flux:select>
-                    </div>
-
-                    <flux:button href="/posts/create" icon="pencil-square" size="sm" variant="primary">{{ __('New post') }}</flux:button>
-                </div>
-            </div>
-
+        <div class="max-sm:pt-8 max-sm:pb-16 pt-12 pb-24">
             <div class="mx-auto max-w-7xl px-6 sm:px-8">
-                <div class="max-sm:pt-8 max-sm:pb-16 pt-12 pb-24">
-                    <div id="posts" class="grid gap-4">
-                        @foreach ($this->posts as $post)
-                            @include('partials.post', ['post' => $post])
+                <div class="flex justify-between">
+                    <flux:heading size="xl">{{ __('All posts') }}</flux:heading>
+
+                    <flux:button href="/posts/create" icon="plus" variant="primary">{{ __('New post') }}</flux:button>
+                </div>
+                <div class="min-h-8"></div>
+                <div class="flex items-center gap-2 justify-end">
+                    <flux:select wire:model.live="board" variant="listbox" class="sm:max-w-fit">
+                        <x-slot name="trigger">
+                            <flux:select.button>
+                                <flux:icon.funnel variant="micro" class="mr-2 text-zinc-400" />
+                                <flux:select.selected />
+                            </flux:select.button>
+                        </x-slot>
+                        <flux:select.option value="all" selected>{{ __('All') }}</flux:select.option>
+                        @foreach ($this->boards as $board)
+                        <flux:select.option :value="$board->id">{{ $board->name }}</flux:select.option>
                         @endforeach
-                    </div>
+                    </flux:select>
+
+                    <flux:select wire:model.live="sort" variant="listbox" class="sm:max-w-fit">
+                        <x-slot name="trigger">
+                            <flux:select.button>
+                                <flux:icon.arrows-up-down variant="micro" class="mr-2 text-zinc-400" />
+                                <flux:select.selected />
+                            </flux:select.button>
+                        </x-slot>
+                        <flux:select.option value="top" selected>{{ __('Top') }}</flux:select.option>
+                        <flux:select.option value="newest">{{ __('Newest') }}</flux:select.option>
+                    </flux:select>
+                </div>
+
+                <div class="min-h-6"></div>
+
+                <div id="posts" class="grid gap-4">
+                    @foreach ($this->posts as $post)
+                        @include('partials.post', ['post' => $post])
+                    @endforeach
                 </div>
             </div>
         </div>
