a2c2009a4ef2a9fff98bd2fa7d78ed23f3624008
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-06-07 14:47:01 -0600

    wip

---
+++ resources/views/pages/demo.blade.php
@@ -0,0 +1,9 @@
+<div>
+<form action="https://fieldgoal.io/f/7o1XVd" method="POST">
+<input type="text" name="first_name">
+<input type="text" name="last_name">
+<input type="text" name="email">
+<input type="text" name="subject">
+<button type="submit">Submit</button>
+</form>
+</div>
